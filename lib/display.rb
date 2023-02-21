d0c2bffdac4398424da51fcf98b2151ae58406f8
Victor <holmstrom.victor@gmail.com>
2023-02-21 22:42:34 +0200

    Update and correct spelling

--- lib/display.rb
+++ lib/display.rb
@@ -7,9 +7,9 @@ class Display
   def new_game
     puts 'New game started.'
     puts 'In this version of mastermind the numbers are assigned to colors, guess the code using numbers'
-    puts 'You have 12 turns to guess the code, will recieve feedback on your guesses:'
+    puts 'You have 12 turns to guess the four digit code, you will recieve feedback on your guesses:'
     puts '   An O means that your guess has a number in the correct position'
-    puts '   An X means that the code contains one of the numbers, but not in the guessed position/n'
+    puts '   An X means that the code contains one of the numbers, but not in the correct position'
     puts ''
   end
 
@@ -20,7 +20,7 @@ class Display
     guess = ''
     until valid_guess
       guess = prompt('Guess the four number combination, or type exit to quit')
-      if handle_input(guess)
+      if valid_input?(guess)
         valid_guess = true
       elsif guess == 'exit'
         valid_guess = true
@@ -38,7 +38,7 @@ class Display
   end
 
   def faulty_input
-    puts 'Not a valid input, remember to only write four numbers corresponding to the colors in the code'
+    puts 'Not a valid input, remember to only write four numbers from 1-6'
     puts 'Try again.'
   end
 
@@ -47,7 +47,7 @@ class Display
     puts ''
   end
 
-  def handle_input(guess)
+  def valid_input?(guess)
     arr = guess.to_i.digits
     if arr.length == 4
       arr.all? { |num| num <= 6 && num >= 1}
